86efe7ddb85be19e54b66a466d37f6333d79062d
Dmitrii Belich <dim1999@mail.ru>
2020-04-12 13:46:46 +0300

    feat: Добавить функционал кнопке add

--- scripts/script.js
+++ scripts/script.js
@@ -1,9 +1,28 @@
-let formElement = document.querySelector('.popup__container');
+let formEditElement = document.querySelector('.popup_button_edit').querySelector('.popup__container');
 let editButton = document.querySelector('.profile__edit-button');
-let closeButton = document.querySelector('.popup__exit-button');
-let like=document.querySelectorAll('.element__like');
+let closeEditButton = document.querySelector('.popup_button_edit').querySelector('.popup__exit-button');
+let like = document.querySelectorAll('.element__like');
+let addButton = document.querySelector('.profile__add-button');
+let formAddElement = document.querySelector('.popup_button_add').querySelector('.popup__container');
+let closeAddButton = document.querySelector('.popup_button_add').querySelector('.popup__exit-button');
 
-function formSubmitHandler (evt) {
+function formEditOpen () {
+  let name = document.querySelector('.profile__name');
+  let subtitle = document.querySelector('.profile__subtitle'); 
+  let nameInput = document.querySelector('.popup_input__el_name');
+  let jobInput = document.querySelector('.popup_input__el_subtitle');
+  nameInput.value = name.textContent;
+  jobInput.value = subtitle.textContent;
+  let open = document.querySelector('.popup_button_edit');
+  open.classList.add ('popup_display_opened');
+}
+
+function formEditClose () {
+  let close = document.querySelector('.popup_button_edit');
+  close.classList.remove ('popup_display_opened');
+}
+
+function formEditSubmitHandler (evt) {
     evt.preventDefault();
     let nameInput = document.querySelector('.popup_input__el_name').value;
     let jobInput = document.querySelector('.popup_input__el_subtitle').value;
@@ -21,24 +40,37 @@ function formSubmitHandler (evt) {
     } else {
       name.classList.add('profile__name_size_m');
     }
-    let close = document.querySelector('.popup');
-    close.classList.remove ('popup_display_opened');
+    formEditClose();
 }
 
-function formOpen (){
-  let name = document.querySelector('.profile__name');
-  let subtitle = document.querySelector('.profile__subtitle'); 
-  let nameInput = document.querySelector('.popup_input__el_name');
-  let jobInput = document.querySelector('.popup_input__el_subtitle');
-  nameInput.value = name.textContent;
-  jobInput.value = subtitle.textContent;
-  let open = document.querySelector('.popup');
+function formAddOpen () {
+  let open = document.querySelector('.popup_button_add');
   open.classList.add ('popup_display_opened');
 }
 
-function formClose () {
-  let close = document.querySelector('.popup');
+function formAddClose () {
+  let close = document.querySelector('.popup_button_add');
   close.classList.remove ('popup_display_opened');
+}
+
+function AddCard (file) {
+  let title = document.querySelector('.popup__input_el_title');
+  console.log(title);
+  let elements = document.querySelector('.elements');
+  elements.insertAdjacentHTML('beforeend', `<div class="element">
+  <img src="${file.target.result}" alt="" class="element__image">
+  <h2 class="element__title">${title.value}</h2>
+  <img src="./images/like.svg" alt="" class="element__like">
+  </div>`);
+  formAddElement.reset();
+}
+
+function formAddSubmitHandler (event) {
+  event.preventDefault();
+  let file = document.querySelector('.popup__input_el_image');
+  var fr = new FileReader();
+  fr.readAsDataURL(file.files[0]);
+  fr.addEventListener("load", AddCard);
 }
 
 function likeActive(event) {
@@ -49,10 +81,12 @@ function likeActive(event) {
   }
 }
 
-formElement.addEventListener('submit', formSubmitHandler);
-editButton.addEventListener('click', formOpen);
-closeButton.addEventListener('click', formClose);
+formEditElement.addEventListener('submit', formEditSubmitHandler);
+editButton.addEventListener('click', formEditOpen);
+closeEditButton.addEventListener('click', formEditClose);
 for (let i = 0; i<like.length; i++) {
 like[i].addEventListener('click', likeActive);
 }
-
+addButton.addEventListener('click', formAddOpen);
+formAddElement.addEventListener('submit', formAddSubmitHandler);
+closeAddButton.addEventListener('click', formAddClose);
