0ff7d46eb81b1edd6a72f68b1a1b0d44fcf19912
Dmitrii Belich <dim1999@mail.ru>
2020-06-11 11:48:43 +0300

    fix: Ошибки ревью

--- scripts/script.js
+++ scripts/script.js
@@ -1,11 +1,7 @@
-import {initialCards, config} from './data.js'
+import {initialCards} from './data.js'
 import {Card} from './Card.js'
-import {FormValidator} from './FormValidator.js'
-import {formEdit} from './formEdit.js'
-import {formAdd, cardContainer} from './formAdd.js'
-
-export const editValidator = new FormValidator (config, formEdit)
-export const addValidator = new FormValidator (config, formAdd)
+import {editValidator} from './formEdit.js'
+import {cardContainer, addValidator} from './formAdd.js'
 
 initialCards.forEach((item) => {
   cardContainer.append(new Card (item.name, item.link, '#card').getCard());
--- scripts/utils.js
+++ scripts/utils.js
@@ -1,42 +1,29 @@
-import {editValidator, addValidator} from './script.js'
-
 const exit = document.querySelectorAll('.popup__exit-button');
 const overlays = document.querySelectorAll('.popup__overlay');
-const popups = document.querySelectorAll('.popup');
 
 function resetForm (form) {
+  if (form) {
   setTimeout(() => {form.reset()}, 200);
+  }
 }
 
 export function popupClose (popup) {
-  
   popup.classList.remove ('popup_display_opened');
   document.removeEventListener('keydown', pressEsc);
   const form = popup.querySelector('.popup__container');
-  setTimeout(() => {
-    if (form) {
-      if (form.name === 'add') {
-        addValidator.setDefault(false);
-      } else if (form.name === 'edit') {
-        editValidator.setDefault(true);
-      }
-    }
-  }, 200);
   resetForm(form);
   }
 
-
-function pressEsc (evt) {
-  if (evt.key === 'Escape') {
-    popups.forEach((popup) => {
-      popupClose(popup);
-    })
+function pressEsc(evt) {
+  const openedPopup = document.querySelector('.popup_display_opened')
+    if (evt.key === 'Escape') {
+      popupClose(openedPopup);
+    }
   }
-}
 
 export function popupOpen (popup) {
   popup.classList.add ('popup_display_opened');
-  document.addEventListener('keydown', pressEsc);
+  document.addEventListener('keydown', pressEsc)
 }
 
 exit.forEach((item) => { 
