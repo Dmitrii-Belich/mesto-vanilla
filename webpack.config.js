4f1fd6f2e36c5b5586225d1497d79c90b79f06f1
Dmitrii Belich <dim1999@mail.ru>
2020-06-18 12:11:39 +0300

    style: убрать комментарии

--- webpack.config.js
+++ webpack.config.js
@@ -1,55 +1,47 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-// подключите к проекту mini-css-extract-plugin
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
   entry: { main: "./scripts/script.js" },
   output: {
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, "dist"),
     filename: "main.js",
   },
   module: {
-    rules: [ // rules — это массив правил
-      // добавим в него объект правил для бабеля
+    rules: [
       {
-        // регулярное выражение, которое ищет все js файлы
         test: /\.js$/,
-        // при обработке этих файлов нужно использовать babel-loader
-        loader: 'babel-loader',
-        // исключает папку node_modules, файлы в ней обрабатывать не нужно
-        exclude: '/node_modules/'
-      },     
-      {
-        // регулярное выражение, которое ищет все файлы с такими расширениями
-        test: /\.(png|svg|jpg|gif|woff2|woff)$/,
-        // при обработке этих файлов нужно использовать file-loader
-        loader: 'file-loader'
+        loader: "babel-loader",
+        exclude: "/node_modules/",
       },
-      // аналогично добавьте правило для работы с html
+      {
+        test: /\.(png|svg|jpg|gif|woff2|woff)$/,
+        loader: "file-loader",
+      },
       {
         test: /\.html$/,
-        loader: 'html-loader',
+        loader: "html-loader",
       },
       {
         test: /\.css$/,
         loader: [
           MiniCssExtractPlugin.loader,
           {
-            loader: 'css-loader',
+            loader: "css-loader",
             options: {
-              importLoaders: 1
-            }
+              importLoaders: 1,
+            },
           },
-          'postcss-loader'
+          "postcss-loader",
         ],
       },
-      ]
+    ],
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: './index.html' // путь к файлу index.html
+      template: "./index.html",
     }),
     new MiniCssExtractPlugin(),
-  ] ,
+  ],
 };
