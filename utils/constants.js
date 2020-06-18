cd24341d321db6530b24e1162c51477abcb4b24b
Dmitrii Belich <dim1999@mail.ru>
2020-06-18 15:59:55 +0300

    feat: добавить UserInfo

--- utils/constants.js
+++ utils/constants.js
@@ -4,15 +4,14 @@ import PopupWithForm from "../components/PopupWithForm.js";
 import { initialCardsData, config } from "../utils/data.js";
 import { Card } from "../components/Card.js";
 import Section from "../components/Section.js";
+import UserInfo from "../components/UserInfo.js"
 
 export const editButton = document.querySelector(".profile__edit-button");
 export const addButton = document.querySelector(".profile__add-button");
 const formAdd = document.forms.add;
-const cardContainer = document.querySelector(".card__container");
-const name = document.querySelector(".profile__name");
-const job = document.querySelector(".profile__subtitle");
 const formEdit = document.forms.edit;
 const initialCards = [];
+const profileInfo = new UserInfo ({nameSelector: ".profile__name", jobSelector: ".profile__subtitle"})
 initialCardsData.forEach((item) => {
   initialCards.push(new Card(item.name, item.link, "#card").getCard());
 });
@@ -56,17 +55,17 @@ export const editPopup = new PopupWithForm(".popup_target_edit", {
   formSubmitHandler: function (evt) {
     evt.preventDefault();
     this._getInputValues();
-    name.textContent = this._inputValues.forename;
-    job.textContent = this._inputValues.job;
+    profileInfo.setUserInfo({name: this._inputValues.forename, job: this._inputValues.job})
     this.close();
   },
   specialOpen: function () {
+    const info = profileInfo.getUserInfo()
     this._inputs.find((item) => {
       return item.getAttribute("name") === "forename";
-    }).value = name.textContent;
+    }).value = info.name;
     this._inputs.find((item) => {
       return item.getAttribute("name") === "job";
-    }).value = job.textContent;
+    }).value = info.job; 
     editValidator.setDefault(true);
   },
 });
